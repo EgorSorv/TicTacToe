732c0a4e9337b0017cd12d7d79d55d417121a947
IDDQDGB <158612932+IDDQDGB@users.noreply.github.com>
2024-02-03 17:43:05 +0300

    Description add in GameWindow.java

--- src/main/java/Project/GameWindow.java
+++ src/main/java/Project/GameWindow.java
@@ -4,18 +4,23 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GameWindow extends JFrame {
+    # Расположение и размер окна
     private static final int WINDOW_HEIGHT = 555;
     private static final int WINDOW_WIDTH = 507;
     private static final int WINDOW_POSX = 800;
     private static final int WINDOW_POSY = 300;
 
+    # Кнопки для выхода и запуска игры
     JButton btnStart = new JButton("New Game");
     JButton btnExit = new JButton("Exit");
 
+    # Данный класс задает игровое поле
     Map map;
+    # Экран с настройками игры
     SettingsWindow settings;
 
     GameWindow() {
+        # Создание окна
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocation(WINDOW_POSX, WINDOW_POSY);
         setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
@@ -29,6 +34,7 @@ public class GameWindow extends JFrame {
 
         btnStart.addActionListener(e -> settings.setVisible(true));
 
+        # Расположение кнопок
         JPanel panBottom = new JPanel(new GridLayout(1, 2));
         panBottom.add(btnStart);
         panBottom.add(btnExit);
@@ -37,6 +43,7 @@ public class GameWindow extends JFrame {
         setVisible(true);
     }
 
+    # Запуск игры
     void startNewGame(int mode, int fSzX, int fSzY, int wLen) {
         map.startNewGame(mode, fSzX, fSzY, wLen);
     }
